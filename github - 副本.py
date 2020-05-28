debaa15480f42dff3d8f1b7833ac8941d3578eea
bc-w <46521465+bc-w@users.noreply.github.com>
2020-05-28 09:32:43 +0800

    Add files via upload

--- github - 副本.py
+++ github - 副本.py
@@ -1,27 +1,20 @@
-#import importlib,sys
-#import configparser
-#import os
-#import re
 import smtplib
 import time
-#import sys
-#import traceback
-#from email import encoders
-#from email.header import Header
-#from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-#from email.utils import formataddr, parseaddr
 from time import gmtime, sleep, strftime
 import requests
+import datetime
 import csv
 from lxml import etree
 
 
 class github_serch:
+
     def __init__(self):
         pass
-    def login_github(self,username="bc-w",password="github登录密码"):#登陆Github
+
+    def login_github(self,username="bc-w",password=""):#登陆Github
         #初始化参数
         login_url = 'https://github.com/login'
         session_url = 'https://github.com/session'
@@ -41,51 +34,64 @@ class github_serch:
             #发送数据并登陆
             s.post(session_url,data=user_data)
             s.get('https://github.com/settings/profile')
-            print("登录Github成功,正在检索关键字xxxxxxx")
+            print("登录Github成功,正在检索关键字genilex")
             #keyword = input("输入搜索关键字:")
-            sleep(6)
-            global n
+            sleep(3)
+            global n #搜索计数
             n = 0
             turl = []
             csv_file = open('leak.csv', 'w', encoding='utf-8', newline='')
             writer = csv.writer(csv_file)
-            writer.writerow(['url',"upload time"])
+            writer.writerow(['url',"upload time","username/entry name","filename"])
             f1 = open('上传时间.txt', 'w')
-            for page in range(1,3): #检索1到3页匹配关键词的结果
-                search_code ="https://github.com/search?p="+str(page)+"&q=xxxxxxx&type=Code"
+            for page in range(1,3): #检索1到2页匹配关键词的结果
+                #search_code ="https://github.com/search?q=genilex&type=Code"
+                search_code = "https://github.com/search?p="+str(page)+"&q=&type=Code"
                 resp = s.get(search_code)
                 results_code = resp.text
                 dom_tree_code = etree.HTML(results_code)
+                #print(results_code)
                 # 获取存在信息泄露的链接地址
-                Urls = dom_tree_code.xpath('//div[@class="flex-auto min-width-0 col-10"]/a[2]/@href')
-                #users = dom_tree_code.xpath('//a[@class="text-blod"]/text()')  # 获取用户名
-                datetime = dom_tree_code.xpath('//relative-time/text()')  # 获取上传时间
+                #Urls = dom_tree_code.xpath('//div[@class="flex-auto min-width-0 col-10"]/a[2]/@href') #获取链接地址
+                Urls = dom_tree_code.xpath('//div[@class="f4 text-normal"]/a/@href')
+                users = dom_tree_code.xpath('//div[@class="flex-shrink-0 text-small text-bold"]/a[1]/@href') # 获取用户名
+                datetime = dom_tree_code.xpath('//relative-time/text()') # 获取上传时间
+                #print(datetime)
+                #print(Urls)
+                str1 = "https://github.com"
                 for i in range(len(Urls)):
                     n = n + 1
-                    for url in Urls:
-                        url = "https://github.com"+url
-                        turl.append(url)
-                    writer.writerow([turl[i],datetime[i]])
+                    writer.writerow([str1+str(Urls[i]),datetime[i],users[i]])
                     f1.write(str(datetime[i]) + "\n")
-            print("共搜索到"+str(n)+"个结果，详见附件")
+            print("共搜索到" + str(n) + "个结果，详见附件")
             return s
         except Exception as e:
             print("产生异常")
             print(e)
-            
+
+    def get_timestamp(self): #创建时间戳
+        f3 = open('上传时间.txt', 'r')
+        f4 = open('上传时间戳.txt', 'w')
+        for i in f3:
+            i = i.strip("\n")
+            ti = datetime.datetime.strptime(i, "%b %d, %Y")
+            timeStamp = (time.mktime(ti.timetuple()))
+            f4.write(str(timeStamp) + "\n")
+
     def send_mail(self): #发送邮件
-        f2 = open('上传时间.txt', 'r')
-        str1 = "2019"
+        f2 = open('上传时间戳.txt', 'r')
         n1 = 0
-        date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
-        username = 'wbc19847@163.com'
-        password = '邮箱登录密码'
-        sender = 'wbc19847@163.com'
-        receiver = ["wbc19847@163.com"]
+        date = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        username = '' #邮件地址
+        password = ''#邮件密码
+        sender = 'wangbeichen@genilex.com'
+        receiver = ["wangbeichen@genilex.com"]
         for ff in f2: #检索上传时间
-            if str1 in ff:
-                n1 = +1
-        if n1 == 0 and n <= 13: #判断上传时间与数量
+            i2 = float(ff.strip("\n"))
+            timeStamp2020 = float(1590388819.0)
+            if i2 > timeStamp2020: #判断上传时间
+                n1 = n1 + 1
+        if n1 == 0: #判断上传数量
             try:
                 subject = 'Github未发现新上传代码'
                 msg = MIMEMultipart('mixed')
@@ -95,7 +101,7 @@ class github_serch:
                 att1["Content-Type"] = 'application/octet-stream'
                 att1.add_header('Content-Disposition', 'attachment', filename='fujian.csv')
                 msg.attach(
-                    MIMEText("Dear all: \r\n\r\n"+str(date)+" 检测关键字xxxxxxx,Github检测到上传代码"+str(n)+"个,未发现新上传代码，详见附件\r\n\r\n"))
+                    MIMEText("Dear all: \r\n\r\n"+str(date)+" 检测关键字,Github检测到上传代码"+str(n)+"个,未发现新上传代码，详见附件\r\n\r\n"))
                 msg.attach(att1)
                 smtp = smtplib.SMTP()
                 smtp.connect('partner.outlook.cn')
@@ -117,11 +123,11 @@ class github_serch:
                 att1["Content-Type"] = 'application/octet-stream'
                 att1.add_header('Content-Disposition', 'attachment', filename='fujian.csv')
                 msg.attach(
-                    MIMEText("Dear all: \r\n\r\n" + str(date) + " 检测关键字xxxxxxx,Github检测到上传代码" + str(
-                        n1) + "个,发现新上传代码，详见附件\r\n\r\n"))
+                    MIMEText("Dear all: \r\n\r\n" + str(date) + " 检测关键字genilex,发现新上传代码，Github检测到新上传代码" + str(
+                        n1) + "个,详见附件\r\n\r\n"))
                 msg.attach(att1)
                 smtp = smtplib.SMTP()
-                smtp.connect('smtp.163.com')
+                smtp.connect('partner.outlook.cn')
                 smtp.starttls()
                 smtp.login(username, password)
                 smtp.sendmail(sender, receiver, msg.as_string())
@@ -129,9 +135,10 @@ class github_serch:
                 print("邮件发送成功")
             except Exception as err:
                 print(err)
-                
+
     def run(self):
         g.login_github()
+        g.get_timestamp()
         g.send_mail()
        
 if __name__ == '__main__':
